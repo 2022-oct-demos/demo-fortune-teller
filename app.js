3b67c2ed43e7634f233961798f3b69b403a0a415
Julie Nisbet <jmnisbet@gmail.com>
2022-10-27 12:35:54 -0700

    add random choice functionality

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const submitBtn = document.getElementById('submit');
 const promptSection = document.getElementById('prompt');
 const fortuneSection = document.getElementById('fortune');
-
+const answerP = document.getElementById('answer');
 /* Events */
 
 //  function examples
@@ -23,9 +23,39 @@ const myName = 'Julia';
 const thing = doSomething;
 console.log(thing());
 
+const answers = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
+
 submitBtn.addEventListener('click', () => {
     // add the hide class to prompt
     promptSection.classList.toggle('hide');
     // remove the hide class from fortune
     fortuneSection.classList.toggle('hide');
+
+    // generate a random number between 0 and the length
+    // of the array minus 1
+    const randInt = Math.floor(Math.random() * answers.length);
+    const randomChoice = answers[randInt];
+
+    // set the text content to the random choice
+    answerP.textContent = randomChoice;
 });
